3b79396fc7b46baa085474cca27ddea8a8796d60
madlings <nagalingam.s@gmail.com>
2026-02-17 21:09:32 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,14 +2,17 @@ from app.players.player import Player
 from app.elves.elf import Elf
 from app.dwarves.dwarf import Dwarf
 
+
 def calculate_team_total_rating(players: list[Player]) -> int:
-  total_rating = sum(player.get_rating() for player in players)
-  return total_rating
+    total_rating = sum(player.get_rating() for player in players)
+    return total_rating
+
 
 def elves_concert(elves: list[Elf]) -> None:
     for elf in elves:
-      elf.play_elf_song()
+        elf.play_elf_song()
+
 
 def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
-  for dwarf in dwarves:
-    dwarf.eat_favourite_dish()
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
