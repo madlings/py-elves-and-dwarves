f24d682cbfb80080c798a138dac4e7578e10a80d
madlings <nagalingam.s@gmail.com>
2026-02-17 19:11:59 +0530

    Update druid.py

--- app/elves/druid.py
+++ app/elves/druid.py
@@ -2,12 +2,18 @@ from app.elves.elf import Elf
 
 
 class Druid(Elf):
-  def __init__(self, nickname: str, musical_instrument: str, favourite_spell: str) -> None:
-    super().__init__(nickname, musical_instrument)
-    self._favorite_spell = favorite_spell
-    
-  def get_rating(self) -> int:
-    return len(self._favourite_spell)
-  
-  def player_info(self) -> str:
-    return f"Druid {self.nickname}. {self.nickname} has a favourite spell: {self._favourite_spell}"
+    def __init__(self,
+                 nickname: str,
+                 musical_instrument: str,
+                 favourite_spell: str) -> None:
+      super().__init__(nickname, musical_instrument)
+      self._favourite_spell = favourite_spell
+
+    def get_rating(self) -> int:
+      return len(self._favourite_spell)
+
+    def player_info(self) -> str:
+      return (
+            f"Druid {self.nickname}. {self.nickname} "
+            f"has a favourite spell: {self._favourite_spell}"
+        )
