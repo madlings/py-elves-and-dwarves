f5e9bae68d918cba2c9fda98440c4967531b5079
madlings <nagalingam.s@gmail.com>
2026-02-17 21:26:21 +0530

    Update druid.py

--- app/elves/druid.py
+++ app/elves/druid.py
@@ -6,14 +6,14 @@ class Druid(Elf):
                  nickname: str,
                  musical_instrument: str,
                  favourite_spell: str) -> None:
-      super().__init__(nickname, musical_instrument)
-      self._favourite_spell = favourite_spell
+        super().__init__(nickname, musical_instrument)
+        self._favourite_spell = favourite_spell
 
     def get_rating(self) -> int:
-      return len(self._favourite_spell)
+        return len(self._favourite_spell)
 
     def player_info(self) -> str:
-      return (
+        return (
             f"Druid {self.nickname}. {self.nickname} "
             f"has a favourite spell: {self._favourite_spell}"
         )
