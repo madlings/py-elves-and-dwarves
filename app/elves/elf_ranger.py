d87f5fc5992feea88ca18ffd23fcbaeb821fc4aa
madlings <nagalingam.s@gmail.com>
2026-02-17 21:11:54 +0530

    Update elf_ranger.py

--- app/elves/elf_ranger.py
+++ app/elves/elf_ranger.py
@@ -6,14 +6,14 @@ class ElfRanger(Elf):
                  nickname: str,
                  musical_instrument: str,
                  bow_level: int) -> None:
-      super().__init__(nickname, musical_instrument)
-      self._bow_level = bow_level
+        super().__init__(nickname, musical_instrument)
+        self._bow_level = bow_level
 
     def get_rating(self) -> int:
-      return 3 * self._bow_level
+        return 3 * self._bow_level
 
     def player_info(self) -> str:
-      return (
-          f"Elf ranger {self.nickname}. "
-          f"{self.nickname} has bow of the {self._bow_level} level"
-      )
+        return (
+            f"Elf ranger {self.nickname}. "
+            f"{self.nickname} has bow of the {self._bow_level} level"
+        )
