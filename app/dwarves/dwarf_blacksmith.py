3d389a95d02ac0892df64dfdb8435e568e970490
madlings <nagalingam.s@gmail.com>
2026-02-17 19:18:24 +0530

    Update dwarf_blacksmith.py

--- app/dwarves/dwarf_blacksmith.py
+++ app/dwarves/dwarf_blacksmith.py
@@ -2,12 +2,18 @@ from app.dwarves.dwarf import Dwarf
 
 
 class DwarfBlacksmith(Dwarf):
-  def __init__(self, nickname: str, favourite_dish: str, skill_level: int) -> None:
-    super().__init__(nickname, favorite_dish)
-    self._skill_level = skill_level
+    def __init__(self,
+                 nickname: str,
+                 favourite_dish: str,
+                 skill_level: int) -> None:
+      super().__init__(nickname, favourite_dish)
+      self._skill_level = skill_level
 
-  def get_rating(self) -> str:
-    return self._skill_level
+    def get_rating(self) -> str:
+      return self._skill_level
 
-  def player_info(self) -> str:
-    return f"Dwarf blacksmith {self.nickname} with skill of the {self._skill_level} level"
+    def player_info(self) -> str:
+      return (
+        f"Dwarf blacksmith {self.nickname} with "
+        f"skill of the {self._skill_level} level"
+      )
